5dcfaf87292c24506e92799a07ae55622b9825b0
Bob <bobylevnet@gmail.com>
2021-03-25 10:47:44 +0500

    25/03

--- models/Filesmodel.go
+++ models/Filesmodel.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"archive/zip"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -18,13 +20,32 @@ type Filesmodel struct {
 	DateCreate string `db:"date_create"`
 	DateDelete string `db:"date_delete"`
 	PathFile   string `db:"path_file"`
-	IDUser     string `db:"id_user"`
-	IdTarget   string `db:"id_target"`
+	IDUser     int    `db:"id_user"`
+	IDTarget   int    `db:"id_target"`
 	Uploaded   bool   `db:"uploaded"`
 }
 
-func FilesUpload(b *http.Request, iduser string, idtarget string) bool {
+type Usersender []struct {
+	IDuser int `json:"IDuser"`
+}
+type Userfiles struct {
+	Filesid int `db:"files_id"`
+	Userid  int `db:"user_id"`
+}
 
+type Username struct {
+	Nameuser string `db:"name_user"`
+}
+
+type Targetname struct {
+	Targetname string `db:"target_name"`
+}
+
+func FilesUpload(b *http.Request, iduser int, idtarget int) bool {
+
+	var usrsndr Usersender
+	//путь сохранения файлов
+	p := createFolder(iduser, idtarget)
 	e := godotenv.Load()
 	if e != nil {
 		fmt.Print(e)
@@ -33,7 +54,10 @@ func FilesUpload(b *http.Request, iduser string, idtarget string) bool {
 	b.ParseMultipartForm(32 << 20)
 
 	m := b.MultipartForm
+	//
+	usersender := []byte(m.Value["usersender"][0])
 
+	json.Unmarshal(usersender, &usrsndr)
 	//текущая дата
 	currentTime := time.Now()
 	tm := fmt.Sprintf(currentTime.Format("01.02.2006"))
@@ -42,26 +66,51 @@ func FilesUpload(b *http.Request, iduser string, idtarget string) bool {
 		for _, f := range v {
 
 			file, err := f.Open()
+			//p := os.Getenv("pathupload")
+			//	zipFiles(p + f.Filename)
 			var outfile *os.File
-			p := os.Getenv("pathupload")
-			if outfile, err = os.Create(p + f.Filename); nil != err {
-				//	status = http.StatusInternalServerError
+			var pthfile = p + f.Filename + ".zip"
+			if outfile, err = os.Create(pthfile); nil != err {
 				log.WriteError(err)
 			}
 
+			zipWriter := zip.NewWriter(outfile)
+			log.WriteError(err)
+
+			info, err := outfile.Stat()
+			header, err := zip.FileInfoHeader(info)
+
+			header.Name = f.Filename
+
+			header.Method = zip.Deflate
+			writer, err := zipWriter.CreateHeader(header)
+
 			var written int64
-			if written, err = io.Copy(outfile, file); nil != err {
+			if written, err = io.Copy(writer, file); nil != err {
 				//return
 			} else {
 				sql = `INSERT INTO tr_files (name_file,  date_create,  date_delete, path_file, id_user, id_target) 
 				VALUES (:name_file,  :date_create,  :date_delete, :path_file, :id_user, :id_target) returning id;`
 
-				upl := Filesmodel{NameFile: f.Filename, DateCreate: tm, DateDelete: tm, PathFile: p + f.Filename, IDUser: iduser, IdTarget: idtarget}
+				upl := Filesmodel{NameFile: f.Filename, DateCreate: tm, DateDelete: tm, PathFile: pthfile, IDUser: iduser, IDTarget: idtarget}
 				r, err := db.Mgr.GetDB().NamedQuery(sql, upl)
-
+				log.WriteError(err)
 				for r.Next() {
 					r.Scan(&upl.ID)
 				}
+
+				/*зполняем структуру приндлежности файла пользователям*/
+
+				for _, v := range usrsndr {
+					oneuserfiles := Userfiles{
+						Userid:  v.IDuser,
+						Filesid: upl.ID,
+					}
+					_, err = db.Mgr.GetDB().NamedExec(`INSERT INTO tr_user_files (files_id,  user_id) 
+				VALUES (:files_id,  :user_id);`, oneuserfiles)
+				}
+
+				log.WriteError(err)
 
 				if err != nil {
 					log.WriteError(err)
@@ -85,6 +134,20 @@ func FilesUpload(b *http.Request, iduser string, idtarget string) bool {
 	return true
 }
 
+func createFolder(iduser int, idtarget int) string {
+	log.WriteError(err)
+
+	trgname := Targetname{}
+	usr := Username{}
+	err = db.Mgr.GetDB().Get(&usr, `SELECT name_user FROM tr_users WHERE id=$1`, iduser)
+	err = db.Mgr.GetDB().Get(&trgname, `SELECT target_name FROM tr_target WHERE id_target=$1`, idtarget)
+	p := os.Getenv("pathupload")
+	os.MkdirAll(p+`/`+usr.Nameuser+`/`+trgname.Targetname, os.ModePerm)
+	path := p + `/` + usr.Nameuser + `/` + trgname.Targetname + `/`
+	return path
+
+}
+
 func FilesUpdate() bool {
 	return true
 }
@@ -105,7 +168,6 @@ func FilesSelect(id_user string, uploaded bool) string {
 	}
 
 	var fls []*Filesmodel
-	//var files []Filesmodel
 	var result = ""
 	rows, err := db.Mgr.GetDB().NamedQuery(`SELECT * FROM tr_files where id_user=:id_user and uploaded=:uploaded`, arg)
 
